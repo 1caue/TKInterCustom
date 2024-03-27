2c8b1139a3988d0355f0ef9e0fa82e9a1ccefa13
Cauê <cauusaantos@gmail.com>
2024-03-26 22:27:56 -0300

    test

--- Cron/cron.py
+++ Cron/cron.py
@@ -22,15 +22,30 @@ def cron():
             lbl2.configure(text=f':{m:02}')
         lbl3.configure(text=f':{s:02}')
         lbl3.after(1000, cron)  
+        if not stop_flag:
+             lbl3.after(100000, cron)
 
 def start_cron():
-    global stop_flag
-    stop_flag = True
-    cron()
+    global stop_flag, a
+    stop_flag = False
+    a += 1
+    if a == 1:
+        cron()       
+    else:
+        messagebox.showinfo('Atenção!', 'O Cronômetro já iniciado!', icon='info')  
 
 def stop():
     global stop_flag
-    stop_flag = False
+    stop_flag = True
+
+def reset():
+     global s, m, h
+     s = 0
+     m = 0
+     h = 0
+     lbl1.configure(text='00')
+     lbl2.configure(text=':00')
+     lbl3.configure(text=':00')   
 
 # Butons
 bt1 = CTkButton(app, text='Comecar', fg_color='#8C8C8C', hover_color='#272727', border_color='black', width=100, border_width=1, command=start_cron)
@@ -39,7 +54,7 @@ bt1.place(relx=0.01, rely=0.8)
 bt2 = CTkButton(app, text='Parar', fg_color='#8C8C8C', hover_color='red', border_color='black', width=104, border_width=1, command=stop)
 bt2.place(relx=0.34, rely=0.8) 
 
-bt3 = CTkButton(app, text='Recomeçar', fg_color='#8C8C8C', hover_color='#272727', border_color='black', width=100, border_width=1, command=start_cron)
+bt3 = CTkButton(app, text='Recomeçar', fg_color='#8C8C8C', hover_color='#272727', border_color='black', width=100, border_width=1, command=reset)
 bt3.place(relx=0.68, rely=0.8)
 
 # Labels
@@ -52,8 +67,9 @@ lbl2.place(relx=0.36, rely=0.3)
 lbl3 = CTkLabel(app, text=':00', text_color='black', font=('Arial', 50))
 lbl3.place(relx=0.58, rely=0.3)
 
-stop_flag = True
+stop_flag = False
 
+a = 0
 s = 0
 m = 0
 h = 0
